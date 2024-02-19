df8879ccc4b8a62ca68b85fa25eb3e20509c28c5
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2024-02-19 20:26:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,26 +19,20 @@ API_ID = int(getenv("API_ID", "6"))
 API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 
 ## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "5516541619:AAHBZEjFu39s67Ce-WgPOHf__LORUF3LpwU")
+BOT_TOKEN = getenv("BOT_TOKEN", "6259808692:AAE7Lt1J2CLvm2A0tN-KRgtB8GqyREmCCD4")
 
 # SUDO USERS
 SUDO_USER = list(
-    map(int, getenv("SUDO_USER", "5533713462").split())
+    map(int, getenv("SUDO_USER", "6609194119").split())
 )  # Input type must be interger
 
 # ADMIN USERS
 ADMIN_USER = list(
-    map(int, getenv("ADMIN_USER", "5533713462 5520828938 817088672").split())
+    map(int, getenv("ADMIN_USER", "6609194119").split())
 )  # Input type must be interger
-
-# You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001766267315"))
 
 # Message to display when someone starts your bot
 PRIVATE_START_MESSAGE = getenv(
     "PRIVATE_START_MESSAGE",
-    "Hello! Welcome to SasteMemes Bot Send only dark/dank memes if you caught sending cringe memes you will be blocked from the bot",
+    "Hello! Welcome to Cheems Feedback Bot, Send feedback about our bots here",
 )
-
-# Database to save your chats and stats... Get MongoDB:-  https://notreallyshikhar.gitbook.io/yukkimusicbot/deployment/mongodb#4.-youll-see-a-deploy-cloud-database-option.-please-select-shared-hosting-under-free-plan-here
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://meme:meme@cluster0.fohmge4.mongodb.net/?retryWrites=true&w=majority")
