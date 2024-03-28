98813235ba89ae3cf35d2387a17caa5570003620
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2024-03-28 17:21:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,342 @@ from pyrogram import __version__ as pyrover
 from pyrogram import filters, idle
 from pyrogram.errors import FloodWait
 from pyrogram.types import Message
-
+import pyrogram, asyncio, os, uvloop, humanfriendly, time
+from pyrogram import Client, filters, idle, enums
+from pyrogram.types import Message
+from sys import version as pyver
+from pyrogram import __version__ as pyrover
 import config
-
+from tools import get_data, fetch_download_link_async, extract_links, check_url_patterns_async, download_file, download_thumb, get_duration, update_progress
+from pyrogram.errors import FloodWait, UserNotParticipant
 uvloop.install()
+import motor.motor_asyncio
 loop = asyncio.get_event_loop()
-SUDO_USERS = config.SUDO_USER
-ADMIN_USERS = config.ADMIN_USER
+
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://video:video@cluster0.suiny.mongodb.net/")
+db = client.rest  # Replace "your_database" with the name of your MongoDB database
+file_collection = db.file
+usersdb = db.users
+
+API_ID = "6"
+API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+BOT_TOKEN = "7186164550:AAFf4kpmfGdY785CeX4FB9SQpDlitPQW-Bc"
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    readable_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", " days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        readable_time += time_list.pop() + ", "
+    time_list.reverse()
+    readable_time += ": ".join(time_list)
+    return readable_time 
 
 app = pyrogram.Client(
-    ":YukkiBot:",
+    "ChatBot",
     config.API_ID,
     config.API_HASH,
     bot_token=config.BOT_TOKEN,
 )
 
+app2 = pyrogram.Client(
+    "acha",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+START_TIME = time.time()
+SUDO_USERS = config.SUDO_USER
+ADMIN_USERS = config.ADMIN_USER
 save = {}
-grouplist = 1
+
+async def remove_file(unique_id):
+    await file_collection.delete_one({'unique_id': unique_id})
+
+async def get_file(unique_id):
+    file = await file_collection.find_one({'unique_id': unique_id})
+    if file:
+        return file.get('file_id')
+    else:
+        return None
+
+async def store_file(unique_id, file_id):
+    file = await file_collection.find_one({'unique_id': unique_id})
+    if file:
+      return
+    await file_collection.insert_one({'unique_id': unique_id, 'file_id': file_id})
+  
+async def add_served_user(user_id: int):
+        is_served = await usersdb.find_one({"user_id": user_id})
+        if is_served:
+            return
+        return await usersdb.insert_one({"user_id": user_id})
+
+async def get_served_users() -> list:
+        users_list = []
+        async for user in usersdb.find({"user_id": {"$gt": 0}}):
+            users_list.append(user)
+        return users_list
+
+
+async def is_join(user_id):
+    try:
+        await app.get_chat_member(-1001885839902, user_id)  
+        await app.get_chat_member(-1001922006659, user_id)
+        return True
+    except UserNotParticipant:
+        return False  
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+
+
+
+@app2.on_message(filters.command("start") & filters.private)
+async def start_fun(client, message: Message):
+    if len(message.command) > 1 and "unqid" in message.command[1]:              
+             unq_id = message.command[1].replace("unqid", "")
+             file_id = await get_file(unq_id)
+             if file_id:
+                 hel = await app.send_cached_media(message.chat.id, file_id)
+                 return await add_served_user(message.chat.id)
+    await message.reply_text("Send Only Terabox urls")
+    return await add_served_user(message.chat.id)
+
+
+
+@app2.on_message(filters.command("stats") & filters.private & filters.user(SUDO_USERS))
+async def stats_func(_, message: Message):
+        if db is None:
+            return await message.reply_text(
+               "MONGO_DB_URI var not defined. Please define it first"
+            )
+        served_users = await db.users.count_documents({})
+        text = f""" **TeraBox Bot Stats:**
+        
+**Python Version :** {pyver.split()[0]}
+**Pyrogram Version :** {pyrover}
+**Served Users:** {served_users}
+**Uptime:** {get_readable_time(time.time() - START_TIME)}"""
+        await message.reply_text(text)
+
+@app2.on_message(filters.command("broadcast") & filters.private & filters.user(SUDO_USERS))
+async def broadcast_func(_, message: Message):
+    if db is None:
+            return await message.reply_text(
+               "MONGO_DB_URI var not defined. Please define it first"
+            )
+    if message.reply_to_message:
+        x = message.reply_to_message.id
+        y = message.chat.id
+    elif len(message.command) < 2:
+        return await message.reply_text(
+            "**Usage**:\n/broadcast [MESSAGE] or [Reply to a Message]"
+        )
+    else:
+        query = message.text.split(None, 1)[1]
+    susr = 0
+    susers = await get_served_users()
+    served_users = [int(user["user_id"]) for user in susers]
+    for i in served_users:
+        try:
+            await app.forward_messages(
+                i, y, x
+            ) if message.reply_to_message else await app.send_message(
+                i, text=query
+            )
+            susr += 1
+        except FloodWait as e:
+            flood_time = int(e.value)
+            await asyncio.sleep(flood_time)
+        except Exception:
+            pass
+    try:
+        await message.reply_text(
+            f"**Broadcasted Message to {susr} Users.**"
+        )
+    except:
+        pass
+
+
+@app2.on_message(filters.chat(-1002089387849) & (filters.text | filters.caption))
+async def message_handler(client, message):
+  text = message.text or message.caption
+  if "tera" in text or "box" in text:
+       asyncio.create_task(terabox_func(client, message))
+
+
+def box_fil(_, __, message):
+    if message.chat.type == enums.ChatType.PRIVATE and (message.text or message.caption):
+        text = message.text or message.caption
+        return "tera" in text or "box" in text
+
+box_filter = filters.create(box_fil)
+
+@app2.on_message(box_filter)
+async def private_message_handler(client, message):
+        asyncio.create_task(terabox_dm(client, message))
+
+
+async def terabox_func(client, message):
+        urls = extract_links(message.text)
+        if not urls:
+          return await message.reply_text("No Urls Found")
+        try:
+            for url in urls:
+                if not await check_url_patterns_async(str(url)):
+                    await message.reply_text("⚠️ Not a valid Terabox URL!", quote=True)
+                    continue
+                try:
+                    await app.send_message(message.from_user.id, ".")
+                except:
+                    return await message.reply_text("First start me in private", quote=True)
+                nil = await message.reply_text("🔎 Processing URL...", quote=True)
+                link_data = await fetch_download_link_async(url)
+                for link in link_data:
+                    name, size, url, thumb  = await get_data(link)
+                    if url:
+                      try:
+                         ril = await client.send_video(message.from_user.id, url, has_spoiler=True, caption=f"**Title**: `{name}`\n**Size**: `{size}`")
+                         file_id = ril.video.file_id
+                         unique_id = ril.video.file_unique_id
+                         await store_file(unique_id, file_id)
+                         direct_url = f"https://t.me/terabox_down_bot?start=unqid{unique_id}"
+                         await nil.edit_text(f"Completed\n\n**File Direct Link:** [Link]({direct_url})", disable_web_page_preview=True)
+                      except FloodWait as e:
+                         await asyncio.sleep(e.value)
+                      except Exception as e:
+                         print(e)
+                         try:
+                            size_bytes = humanfriendly.parse_size(size)
+                            if int(size_bytes) < 900 * 1024 * 1024 and name.lower().endswith(('.mp4', '.mkv', '.webm', '.Mkv')):
+                               vid_path = await loop.run_in_executor(None, download_file, url, name)
+                               thumb_path = await loop.run_in_executor(None, download_thumb, thumb)
+                               dur = await loop.run_in_executor(None, get_duration, vid_path)                                                                 
+                               ril = await client.send_video(message.from_user.id, vid_path, has_spoiler=True, thumb=thumb_path, caption=f"**Title**: `{name}`\n**Size**: `{size}`", duration=int(dur))
+                               file_id = ril.video.file_id
+                               unique_id = ril.video.file_unique_id
+                               await store_file(unique_id, file_id)
+                               direct_url = f"https://t.me/terabox_down_bot?start=unqid{unique_id}"
+                               await nil.edit_text(f"Completed\n\n**File Direct Link:** [Link]({direct_url})", disable_web_page_preview=True)
+                            else:
+                                await client.send_photo(message.from_user.id, thumb, has_spoiler=True, caption=f"**Title**: `{name}`\n**Size**: `{size}`\n**Link**: {url}")
+                                await nil.edit_text("Completed")
+                         except FloodWait as e:
+                              await asyncio.sleep(e.value)
+                         except Exception as e:
+                           print(e)
+                           try:
+                             ril = await client.send_document(message.from_user.id, vid_path, thumb=thumb_path, caption=f"**Title**: `{name}`\n**Size**: `{size}`")
+                             file_id = ril.document.file_id
+                             unique_id = ril.document.file_unique_id
+                             await store_file(unique_id, file_id)
+                             direct_url = f"https://t.me/terabox_down_bot?start=unqid{unique_id}"
+                             await nil.edit_text(f"Completed\n\n**File Direct Link:** [Link]({direct_url})", disable_web_page_preview=True)
+                           except Exception as e: 
+                             print(e)
+                             await client.send_photo(message.from_user.id, thumb, has_spoiler=True, caption=f"**Title**: `{name}`\n**Size**: `{size}`\n**Link**: {url}")
+                             await nil.edit_text("Completed")
+                         finally:
+                                if vid_path and os.path.exists(vid_path):
+                                   os.remove(vid_path)
+                                if thumb_path and os.path.exists(thumb_path):
+                                     os.remove(thumb_path)
+        except FloodWait as e:
+          await asyncio.sleep(e.value)
+        except Exception as e:
+            print(e)
+            await message.reply_text("Some Error Occurred", quote=True)
+
+
+async def terabox_dm(client, message):
+        is_served = await usersdb.find_one({"user_id": message.chat.id})
+        if not is_served:
+            return await message.reply_text("Type /start first then try sending url again")
+        if not await is_join(message.from_user.id):
+            return await message.reply_text("you need to join @CheemsBackup and @CheemsChat before using me")
+        urls = extract_links(message.text)
+        if not urls:
+          return await message.reply_text("No Urls Found")
+        try:
+            for url in urls:
+                if not await check_url_patterns_async(str(url)):
+                    await message.reply_text("⚠️ Not a valid Terabox URL!", quote=True)
+                    continue              
+                nil = await message.reply_text("🔎 Processing URL...", quote=True)
+                link_data = await fetch_download_link_async(url)
+                for link in link_data:
+                    name, size, url, thumb  = await get_data(link)
+                    if url:
+                      try:
+                         ril = await client.send_video(-1002001643006, url, caption="Indian")
+                         file_id = ril.video.file_id
+                         unique_id = ril.video.file_unique_id
+                         await store_file(unique_id, file_id)
+                         direct_url = f"https://t.me/terabox_down_bot?start=unqid{unique_id}"
+                         await ril.copy(message.chat.id, caption=f"**Title**: `{name}`\n**Size**: `{size}`\n\n**Direct File Link**: {direct_url}")
+                         await nil.edit_text("Completed")
+                      except FloodWait as e:
+                         await asyncio.sleep(e.value)
+                      except Exception as e:
+                         print(e)
+                         try:
+                            size_bytes = humanfriendly.parse_size(size)
+                            if int(size_bytes) < 900 * 1024 * 1024 and name.lower().endswith(('.mp4', '.mkv', '.webm', '.Mkv')):
+                               vid_path = await loop.run_in_executor(None, download_file, url, name)
+                               thumb_path = await loop.run_in_executor(None, download_thumb, thumb)
+                               dur = await loop.run_in_executor(None, get_duration, vid_path)                                                                 
+                               ril = await client.send_video(-1002001643006, vid_path, thumb=thumb_path, duration=int(dur), caption="Indian")
+                               file_id = ril.video.file_id
+                               unique_id = ril.video.file_unique_id
+                               await store_file(unique_id, file_id)
+                               direct_url = f"https://t.me/terabox_down_bot?start=unqid{unique_id}"
+                               await ril.copy(message.chat.id, caption=f"**Title**: `{name}`\n**Size**: `{size}`\n\n**Direct File Link**: {direct_url}")
+                               await nil.edit_text("Completed")
+                            else:
+                                await client.send_photo(message.chat.id, thumb, has_spoiler=True, caption=f"**Title**: `{name}`\n**Size**: `{size}`\n**Link**: {url}")
+                                await nil.edit_text("Completed")
+                         except FloodWait as e:
+                              await asyncio.sleep(e.value)
+                         except Exception as e:
+                           print(e)
+                           try:
+                             ril = await client.send_document(-1002001643006, vid_path, thumb=thumb_path)
+                             file_id = ril.document.file_id
+                             unique_id = ril.document.file_unique_id
+                             await store_file(unique_id, file_id)
+                             direct_url = f"https://t.me/terabox_down_bot?start=unqid{unique_id}"
+                             await ril.copy(message.chat.id, caption=f"**Title**: `{name}`\n**Size**: `{size}`\n\n**Direct File Link**: {direct_url}")
+                             await nil.edit_text("Completed")
+                           except Exception as e: 
+                             print(e)
+                             await client.send_photo(message.chat.id, thumb, has_spoiler=True, caption=f"**Title**: `{name}`\n**Size**: `{size}`\n**Link**: {url}")
+                             await nil.edit_text("Completed")
+                         finally:
+                                if vid_path and os.path.exists(vid_path):
+                                   os.remove(vid_path)
+                                if thumb_path and os.path.exists(thumb_path):
+                                     os.remove(thumb_path)
+        except FloodWait as e:
+          await asyncio.sleep(e.value)
+        except Exception as e:
+            print(e)
+            await message.reply_text("Some Error Occurred", quote=True)
+
+
 
 
 @app.on_message(filters.command(["start", "help"]) & filters.private)
