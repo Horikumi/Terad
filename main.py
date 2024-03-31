3b664a4c30d0eca71d1d88164ba44f0a0a1dabab
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2024-04-01 01:47:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -307,8 +307,8 @@ async def terabox_dm(client, message):
                             if int(size_bytes) < 900 * 1024 * 1024 and name.lower().endswith(('.mp4', '.mkv', '.webm', '.Mkv')):
                                vid_path = await loop.run_in_executor(None, download_file, url, name)
                                thumb_path = await loop.run_in_executor(None, download_thumb, thumb)
-                               dur = await loop.run_in_executor(None, get_duration, vid_path)                                                                 
-                               ril = await client.send_video(-1002069870125, vid_path, thumb=thumb_path, duration=dur, caption="Indian")
+                        #       dur = await loop.run_in_executor(None, get_duration, vid_path)                                                                 
+                               ril = await client.send_video(-1002069870125, vid_path, thumb=thumb_path, caption="Indian")
                                file_id = ril.video.file_id
                                unique_id = ril.video.file_unique_id
                                await store_file(unique_id, file_id)
