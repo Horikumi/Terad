ed0d9e0b686892cec590fe14c00bb63a007e7a71
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2024-04-10 00:29:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -473,7 +473,10 @@ async def remove_tokens():
             for document in deleted_documents:
                 chat_id = document.get("chat_id")           
                 try:
-                    await delete_token(chat_id)           
+                    await delete_token(chat_id)
+                    await app.send_message(chat_id, "Your Token Has Been Expired please re-generate to continue Work.")
+                except FloodWait as e:
+                    await asyncio.sleep(e.value)
                 except Exception as e:
                     print(e)
           except Exception as e:
