27961044203f89d41384d39c90cae756cf5ca827
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2024-04-10 16:08:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ rokendb = db.roken
 
 API_ID = "6"
 API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
-BOT_TOKEN = "7121574962:AAHGKpXiEb22KselVFhIyWxCd-8_ub7m3lg"
+BOT_TOKEN = "7121574962:AAEsnRf_PiLXF6KLAo4R9Uu1siS3Va0gAi8"
 
 queue_url = {}
 joined = set()
