edc59df5b87405a75e90a6612545c74d8fe2341d
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2024-04-10 14:32:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -412,7 +412,7 @@ async def terabox_dm(client, message):
                             await store_file(unique_id, file_id)
                             await store_url(url, file_id, unique_id, direct_url)
                          else:
-                             await client.send_message(message.chat.id, text=f"**Failed To Download Media Try Downloading using Download Link.**\n\n**Title**: `{name}`\n**Size**: `{size}`\n**Download Link**: {dlink}")
+                             await message.reply_text(f"**Failed To Download Media Try Downloading using Download Link.**\n\n**Title**: `{name}`\n**Size**: `{size}`\n**Download Link**: {dlink}", quote=True)
                              await nil.edit_text("Completed")                     
                       except FloodWait as e:
                          await asyncio.sleep(e.value)
@@ -434,7 +434,7 @@ async def terabox_dm(client, message):
                               await asyncio.sleep(e.value)
                          except Exception as e:
                            print(e)                     
-                           await client.send_message(message.chat.id, text=f"**Failed To Download Media Try Downloading using Download Link.**\n\n**Title**: `{name}`\n**Size**: `{size}`\n**Download Link**: {dlink}")
+                           await message.reply_text(f"**Failed To Download Media Try Downloading using Download Link.**\n\n**Title**: `{name}`\n**Size**: `{size}`\n**Download Link**: {dlink}", quote=True)
                            await nil.edit_text("Completed")
                          finally:
                                 if vid_path and os.path.exists(vid_path):
