87b630e6b9b2c27498b14a62a1e7714c7ea37d89
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2024-04-13 14:02:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -392,21 +392,11 @@ async def terabox_func(client, message):
                                 unique_id = (ril.video.file_unique_id if ril.video else (ril.document.file_unique_id if ril.document else (ril.animation.file_unique_id if ril.animation else (ril.sticker.file_unique_id if ril.sticker else (ril.photo.file_unique_id if ril.photo else ril.audio.file_unique_id if ril.audio else None)))))                     
                                 direct_url = f"https://t.me/teradlrobot?start=unqid{unique_id}"
                                 await ril.copy(message.chat.id, caption=f"**Title**: `{name}`\n**Size**: `{size}`\n\n**Direct File Link**: {direct_url}")
-                                await nil.edit_text("Completed")
-                                try:
-                                    os.remove(vid_path)
-                                    os.remove(thumb_path)
-                                except:
-                                    pass
+                                await nil.edit_text("Completed")                                
                                 await store_file(unique_id, file_id)
                                 await store_url(url, file_id, unique_id, direct_url)
                              except Exception as e:
-                                 print(e)
-                                 try:
-                                    os.remove(vid_path)
-                                    os.remove(thumb_path)
-                                 except:
-                                    pass                             
+                                 print(e)                                                         
                                  await client.send_photo(message.chat.id, thumb, has_spoiler=True, caption=f"**Title**: `{name}`\n**Size**: `{size}`\n**Download Link**: {dlink}")
                                  await nil.edit_text("Completed")
                              finally:
