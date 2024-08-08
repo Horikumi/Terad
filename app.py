3f5c50ac9216c791a0757119fedf2acdd49909fb
Horikumi <148572875+Horikumi@users.noreply.github.com>
2024-08-08 18:05:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
+@app.route('/play')
+def play():
+    play_url = request.args.get('play_url')
+    thumb = request.args.get('thumb')
+    return render_template('play.html', play_url=play_url, thumb=thumb)
 
 @app.route('/')
 def hello_world():
     return 'cheems'
-
 
 if __name__ == "__main__":
     app.run()
